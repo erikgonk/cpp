66e8438297af86ddede1bf2e2257d7a60efa2f3c
erikgonk <twire01@gmail.com>
2024-10-16 12:08:45 +0200

    [ex05] color moved to hpp

--- cpp1/ex05/Harl.hpp
+++ cpp1/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: erigonza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:36:16 by erigonza          #+#    #+#             */
-/*   Updated: 2024/10/16 11:42:46 by erigonza         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:08:24 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,18 @@
 
 # include <iostream>
 # include <string>
+
+#define RESET   "\033[0m"
+#define BOLD     "\033[1m"
+#define RED_BACKGROUND "\033[41m"
+#define RED     "\033[31m"
+#define BOLD_RED "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[1;33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
 
 class Harl
 {
--- cpp1/ex05/main.cpp
+++ cpp1/ex05/main.cpp
@@ -6,23 +6,11 @@
 /*   By: erigonza <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 11:35:43 by erigonza          #+#    #+#             */
-/*   Updated: 2024/10/16 12:04:17 by erigonza         ###   ########.fr       */
+/*   Updated: 2024/10/16 12:08:18 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
-
-#define RESET   "\033[0m"
-#define BOLD     "\033[1m"
-#define RED_BACKGROUND "\033[41m"
-#define RED     "\033[31m"
-#define BOLD_RED "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[1;33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
 
 int main( void )
 {
