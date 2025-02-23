00bdafe34212d45269671905b0122613f0d7c387
erikgonk <twire01@gmail.com>
2025-02-23 12:46:21 +0100

    cpp6 correction

--- cpp6/ex00/ScalarConverter.hpp
+++ cpp6/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: erigonza <erigonza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 13:46:24 by erigonza          #+#    #+#             */
-/*   Updated: 2025/02/22 16:59:44 by erigonza         ###   ########.fr       */
+/*   Updated: 2025/02/23 12:43:25 by erigonza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,17 @@
 class ScalarConverter
 {
   public:
-    ScalarConverter();
-    ScalarConverter(const ScalarConverter &);
-    ScalarConverter &operator=(const ScalarConverter &);
-    ~ScalarConverter();
     static void convert(const std::string &literal);
 
   private:
-    ScalarConverter();
-		ScalarConverter(const ScalarConverter &);
-		ScalarConverter &operator=(const ScalarConverter &);
-		~ScalarConverter();
-    static void convertToChar(double value, std::string &str);
-    static void convertToInt(double value, std::string &str);
-    static void convertToFloat(double value, std::string &str);
-    static void convertToDouble(double value, std::string &str);
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter &);
+	ScalarConverter &operator=(const ScalarConverter &);
+	~ScalarConverter();
+	static void convertToChar(double value, std::string &str);
+	static void convertToInt(double value, std::string &str);
+	static void convertToFloat(double value, std::string &str);
+	static void convertToDouble(double value, std::string &str);
 };
 
 #endif
